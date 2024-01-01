ff53dc4d3388dffd3612131e81e529126f13a4ac
Houda Ben Salah <houda.bensalah@polytechnicien.tn>
2024-01-01 16:36:38 +0100

    Add docker-hadoop submodule

---
+++ src/main/java/hadoop/mapreduce/wordcount/HadoopMapreduceApplication.java
@@ -0,0 +1,13 @@
+package hadoop.mapreduce.wordcount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadoopMapreduceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadoopMapreduceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hadoop/mapreduce/wordcount/HadoopMapreduceApplicationTests.java
@@ -0,0 +1,13 @@
+package hadoop.mapreduce.wordcount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadoopMapreduceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
